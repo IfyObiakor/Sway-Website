18aca3346f59c54e0d0bed340418acb97f54c873
IfyObiakor <114431584+IfyObiakor@users.noreply.github.com>
2022-11-28 21:43:00 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+// hamburger
+
+let hamburger = document.querySelector(".hamburger")
+    
+    hamburger.addEventListener("click", function() {
+        document.querySelector("body").classList.toggle("active");
+    })
+
+
+// Sidebar menu
+
+let menu = document.querySelectorAll(".menu")
+menu.forEach((n)=> {
+   n.addEventListener("click", function(){
+      n.classList.toggle("active")
+   })
+})
+
+
+
+// Navbar
+let navbar = document.querySelector(".container2")
+let sticky = navbar.offsetTop;
+
+function position() {
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("fix-my-nav")
+    }else {
+        navbar.classList.remove("fix-my-nav")
+    }
+}
+window.onscroll = function() {
+    position();
+}
